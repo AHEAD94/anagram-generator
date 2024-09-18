96c58ac63fcf25bd723f082485ef90e0d63242bf
AHEAD94 <ulysses94@naver.com>
2024-09-18 17:03:15 +0900

    chore: 프로젝트 초기 설정

---
+++ AnagramGenerator/AnagramGenerator/AnagramGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnagramGeneratorApp.swift
+//  AnagramGenerator
+//
+//  Created by Ryu Dae-ha on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AnagramGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
