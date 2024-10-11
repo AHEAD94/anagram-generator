a26e3eac660cf8fbc6d1c30eea097f77816543b4
AHEAD94 <ulysses94@naver.com>
2024-10-12 01:51:40 +0900

    fix: Anagram 생성 문자열의 길이를 9로 제한

--- AnagramGenerator/AnagramGenerator/Views/GenerationView.swift
+++ AnagramGenerator/AnagramGenerator/Views/GenerationView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct GenerationView: View {
+    // AnagramGenerator는 9개의 문자까지만 애너그램을 생성해야 함
+    // 문자 10개가 넘는 문자열에 대해 애너그램 생성 시, 순열을 계산하는 시간이 매우 길기 때문
+    // 순열을 생성하는 알고리즘은 O(n!)의 시간 복잡도를 보유
     @State private var generator = Generator()
     
     @State private var anagrams: [String] = [""]
     @State private var keyword: String = ""
     @State private var name: String = ""
     @State private var selectedDate = Date()
+    
+    let maxNameLen: Int = 9
     
     var body: some View {
         NavigationView {
@@ -24,7 +29,7 @@ struct GenerationView: View {
                 Section(header: Text("Private info")) {
                     TextField("Enter your name", text: $name)
                         .onChange(of: name) {
-                            name = name.lowercased()  // 입력값을 소문자로 변환
+                            name = name.lowercased() // 입력값을 소문자로 변환
                         }
                     DatePicker("Birthday", selection: $selectedDate, displayedComponents: .date)
                         .foregroundStyle(.secondary)
@@ -37,6 +42,7 @@ struct GenerationView: View {
                 } label: {
                     Text("Generate Anagrams")
                 }
+                .disabled(name.count > maxNameLen)
                 
                 Section(header: Text("Result anagrams")) {
                     List {
